d359ebff5966d91c1c37482372c6f7720a817f18
Yazid <yazyyazid@gmail.com>
2025-06-25 23:14:49 +0800

    code update jupiter

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <p class="mb-0">&copy; <?php echo date('Y'); ?> Jupyter Team. All rights reserved.</p>
+                    <p class="mb-0">&copy; <?php echo date('Y'); ?> Jupiter Team. All rights reserved.</p>
                 </div>
                 <div class="col-md-6 text-md-end">
                     <p class="mb-0">
