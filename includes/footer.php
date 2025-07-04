bdd62c231b02aa9f3d789f893c21b8efa638db32
Yazid <yazyyazid@gmail.com>
2025-07-04 22:26:09 +0800

    remove some footer

--- includes/footer.php
+++ includes/footer.php
@@ -5,13 +5,7 @@
             <div class="row">
                 <div class="col-md-6">
                     <p class="mb-0">&copy; <?php echo date('Y'); ?> Jupiter Team. All rights reserved.</p>
-                </div>
-                <div class="col-md-6 text-md-end">
-                    <p class="mb-0">
-                        <a href="#" class="text-decoration-none">Privacy Policy</a> | 
-                        <a href="#" class="text-decoration-none">Terms of Service</a>
-                    </p>
-                </div>
+                </div> 
             </div>
         </div>
     </footer>
