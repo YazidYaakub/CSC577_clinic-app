b974636bd2be1f68ff8a261ca4e590be80cdc773
Yazid <yazyyazid@gmail.com>
2025-07-12 05:40:59 +0800

    remove add new user in admin

--- admin/users.php
+++ admin/users.php
@@ -544,6 +544,7 @@ include '../includes/header.php';
             </div>
             
         <?php else: ?>
+        <?php /*
             <!-- All Users View --> 
             <div class="card mb-4">
     <div class="card-header d-flex justify-content-between align-items-center">
@@ -652,7 +653,8 @@ include '../includes/header.php';
 </form>
 </div>
     </div>
-</div>      
+    </div>
+         */ ?>    
             <div class="d-flex justify-content-between align-items-center mb-4">
                 <h1>Manage Users</h1>
             </div>
